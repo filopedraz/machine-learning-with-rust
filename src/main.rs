9ac1a6425666e6167888ddc6b5746ed2276c0ef0
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-09-06 09:49:05 +0200

    linfa example with logistic regression

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use machine_learning_with_rust::utils::run_polar_example;
+use machine_learning_with_rust::utils::{run_polar_example, run_linfa_example};
 
 fn main() {
     run_polar_example();
+    run_linfa_example();
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,10 @@
 use polars::prelude::*;
 
+use linfa::prelude::*;
+use linfa::traits::{Fit, Predict};
+use linfa_datasets::winequality;
+use linfa_logistic::LogisticRegression;
+
 pub fn run_polar_example() {
     // Create a DataFrame
     let df = DataFrame::new(vec![
@@ -10,4 +15,15 @@ pub fn run_polar_example() {
     // Filter rows where Age is greater than 25
     let filtered = df.filter(&df["Age"].gt(25).unwrap()).unwrap();
     println!("{:?}", filtered);
+}
+
+pub fn run_linfa_example() {
+    let (train, valid) = winequality()
+        .map_targets(|x| if *x > 6 { "good" } else { "bad" })
+        .split_with_ratio(0.9);
+
+    let model = LogisticRegression::default().max_iterations(150).fit(&train).unwrap();
+    let pred = model.predict(&valid);
+    let cm = pred.confusion_matrix(&valid).unwrap();
+    println!("{:?}", cm);
 }
